494e3d2a5017ac6425232fef0c1f91b652df603d
Juan Carlo Banayo <johncban@gmail.com>
2020-05-23 00:56:58 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -7,6 +7,13 @@ class CouponsController < ApplicationController
     @coupon = Coupon.find(params[:id])
   end
 
+  def new; end
+
   def create
+    @coupon = Coupon.new
+    @coupon[:coupon_code] = params[:coupon][:coupon_code]
+    @coupon[:store] = params[:coupon][:store]
+    @coupon.save
+    redirect_to_coupon_path(@coupon)
   end
 end
